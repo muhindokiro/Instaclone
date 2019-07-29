cf5e15929501da00ca906f7bfd55f1dcc0096f88
muhindokiro <blaize1143@gmail.com>
2019-07-29 11:15:46 +0300

    add a pic class

--- ig/models.py
+++ ig/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
 class Pic(models.Model): 
     image = models.ImageField(upload_to = 'timeline/')
     caption = models.CharField(max_length =30)
