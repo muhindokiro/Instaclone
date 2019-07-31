72d3fa8364e5948233749ad4054bff7ffff591cf
muhindokiro <blaize1143@gmail.com>
2019-07-31 16:42:11 +0300

    made changes

--- instagram/settings.py
+++ instagram/settings.py
@@ -175,8 +175,8 @@ django_heroku.settings(locals())
 
 # Email configurations remember to install python-decouple
 # EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
